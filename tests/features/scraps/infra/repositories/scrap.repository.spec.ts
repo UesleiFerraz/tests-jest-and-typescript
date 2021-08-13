57f8084d77940bc956458e4a2a5be1a75c5d8237
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-13 16:01:47 -0300

    add: Get all scraps 1st test

--- tests/features/scraps/infra/repositories/scrap.repository.spec.ts
+++ tests/features/scraps/infra/repositories/scrap.repository.spec.ts
@@ -1,6 +1,7 @@
 import { User } from "../../../../../src/core/domain";
 import { ScrapEntity, UserEntity } from "../../../../../src/core/infra";
 import Database from "../../../../../src/core/infra/data/connections/database";
+import { Scrap } from "../../../../../src/features/scraps/domain";
 import { ScrapRepository } from "../../../../../src/features/scraps/infra";
 
 const makeUser = async (): Promise<User> => {
@@ -18,6 +19,12 @@ const makeParams = async () => {
     description: "any_description",
     userUid: user.uid,
   };
+};
+
+const makeScrap = async (): Promise<Scrap> => {
+  const params = await makeParams();
+
+  return ScrapEntity.create(params).save();
 };
 
 describe("Scrap repository", () => {
@@ -42,6 +49,21 @@ describe("Scrap repository", () => {
       expect(result.title).toEqual(params.title);
       expect(result.description).toEqual(params.description);
       expect(result.userUid).toEqual(params.userUid);
+    });
+  });
+
+  describe("Get all scraps", () => {
+    it("Should return any scraps on database when call this function", async () => {
+      const params = await makeParams();
+      const sut = new ScrapRepository();
+      await sut.create(params);
+      const result = await sut.getAll(params.userUid);
+
+      expect(result).toBeTruthy();
+      expect(result.length).toBeGreaterThanOrEqual(1);
+      expect(result[0].title).toEqual(params.title);
+      expect(result[0].description).toEqual(params.description);
+      expect(result[0].userUid).toEqual(params.userUid);
     });
   });
 });
