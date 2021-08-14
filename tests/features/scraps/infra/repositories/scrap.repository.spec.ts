39000659b59f926215367573564a981224d9c6e2
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-14 16:51:38 -0300

    add: Delete scrap 3rd test and some small changes

--- tests/features/scraps/infra/repositories/scrap.repository.spec.ts
+++ tests/features/scraps/infra/repositories/scrap.repository.spec.ts
@@ -47,6 +47,7 @@ describe("Scrap repository", () => {
       const result = await sut.create(params as any);
 
       expect(result).toBeTruthy();
+      expect(result.uid).toBeTruthy();
       expect(result.title).toEqual(params.title);
       expect(result.description).toEqual(params.description);
       expect(result.userUid).toEqual(params.userUid);
@@ -62,6 +63,7 @@ describe("Scrap repository", () => {
 
       expect(result).toBeTruthy();
       expect(result.length).toBeGreaterThanOrEqual(1);
+      expect(result[0].uid).toBeTruthy();
       expect(result[0].title).toEqual(params.title);
       expect(result[0].description).toEqual(params.description);
       expect(result[0].userUid).toEqual(params.userUid);
@@ -69,8 +71,8 @@ describe("Scrap repository", () => {
 
     it("Should pass an userUid as a parameter when call this function", async () => {
       const sut = new ScrapRepository();
-      jest.spyOn(sut, "getAll").mockResolvedValue([await makeScrap()]);
       const spy = jest.spyOn(sut, "getAll");
+      spy.mockResolvedValue([await makeScrap()]);
 
       await sut.getAll("any_user_uid");
 
@@ -104,6 +106,7 @@ describe("Scrap repository", () => {
       const result = (await sut.getOne(scrap.uid, scrap.userUid)) as any;
 
       expect(result).toBeTruthy();
+      expect(result.uid).toEqual(scrap.uid);
       expect(result.title).toEqual(scrap.title);
       expect(result.description).toEqual(scrap.description);
       expect(result.userUid).toEqual(scrap.userUid);
@@ -111,8 +114,8 @@ describe("Scrap repository", () => {
 
     it("Should pass an userUid and a scrapUid as parameters when call this function", async () => {
       const sut = new ScrapRepository();
-      jest.spyOn(sut, "getOne").mockResolvedValue(null);
       const spy = jest.spyOn(sut, "getOne");
+      spy.mockResolvedValue(null);
 
       await sut.getOne("any_uid", "any_user_uid");
 
@@ -151,6 +154,7 @@ describe("Scrap repository", () => {
       } as any)) as any;
 
       expect(result).toBeTruthy();
+      expect(result.uid).toEqual(scrap.uid);
       expect(result.title).toEqual(scrap.title);
       expect(result.description).toEqual(scrap.description);
       expect(result.userUid).toEqual(scrap.userUid);
@@ -158,8 +162,8 @@ describe("Scrap repository", () => {
 
     it("Should pass an userUid and a scrap as parameters when call this function", async () => {
       const sut = new ScrapRepository();
-      jest.spyOn(sut, "update").mockResolvedValue(null);
       const spy = jest.spyOn(sut, "update");
+      spy.mockResolvedValue(null);
       const params = await makeParams();
 
       await sut.update("any_uid", params as any);
@@ -180,6 +184,7 @@ describe("Scrap repository", () => {
       const result = (await sut.update(scrap.uid, params as any)) as any;
 
       expect(result).toBeTruthy();
+      expect(result.uid).toEqual(scrap.uid);
       expect(result.title).toEqual(params.title);
       expect(result.description).toEqual(params.description);
       expect(result.userUid).toEqual(params.userUid);
@@ -202,6 +207,15 @@ describe("Scrap repository", () => {
 
       expect(spy).toHaveBeenCalledWith("any_uid", "any_user_uid");
       expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should delete an scrap if all params are valid", async () => {
+      const sut = new ScrapRepository();
+      const scrap = await makeScrap();
+      const result = (await sut.delete(scrap.uid, scrap.userUid)) as any;
+
+      expect(result).toBeTruthy();
+      expect(result.uid).toBeFalsy();
     });
   });
 });
