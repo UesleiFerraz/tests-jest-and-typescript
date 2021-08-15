ffa0559654c2a477d892f80022e7f00960bf6b60
UesleiFerraz <uesleivaleriod@gmail.com>
2021-08-15 20:02:18 -0300

    add: routes scraps 1st test

---
+++ tests/features/scraps/presentation/routes/routes.test.ts
@@ -0,0 +1,76 @@
+import express, { Router } from "express";
+import {
+  CacheRepository,
+  ScrapEntity,
+  UserEntity,
+} from "../../../../../src/core/infra";
+import { Scrap } from "../../../../../src/features/scraps/domain";
+import Database from "../../../../../src/core/infra/data/connections/database";
+import ScrapRoutes from "../../../../../src/features/scraps/presentation/routes/routes";
+import supertest from "supertest";
+import { ScrapRepository } from "../../../../../src/features/scraps/infra";
+import App from "../../../../../src/core/presentation/app";
+import { ok } from "../../../../../src/core/presentation";
+import { UserAuthMiddleware } from "../../../../../src/features/scraps/presentation/middlewares/user-auth.middleware";
+
+const makeUser = async (): Promise<UserEntity> => {
+  return UserEntity.create({
+    username: new Date().getMilliseconds().toLocaleString(),
+    password: "any_password",
+  }).save();
+};
+
+const makeToken = () => {};
+
+const makeScrap = async (): Promise<Scrap> => {
+  const user = await makeUser();
+  return ScrapEntity.create({
+    title: "any_name",
+    description: "any_description",
+    userUid: user.uid,
+  }).save();
+};
+
+describe("Scrap routes", () => {
+  const server = new App().server;
+
+  beforeEach(async () => {
+    await ScrapEntity.delete({});
+    await UserEntity.delete({});
+
+    jest.resetAllMocks();
+  });
+
+  beforeAll(async () => {
+    await new Database().openConnection();
+
+    const router = Router();
+    server.use(express.json());
+
+    server.use(router);
+
+    new ScrapRoutes().init(router);
+  });
+
+  afterAll(async () => {
+    await new Database().disconnectDatabase();
+  });
+
+  describe("/Get scraps", () => {
+    it("Should return all scraps if has any scrap", async () => {
+      const scrap = await makeScrap();
+      jest.spyOn(CacheRepository.prototype, "get").mockResolvedValue([scrap]);
+      jest
+        .spyOn(UserAuthMiddleware.prototype, "handle")
+        .mockResolvedValue(ok({ userUid: scrap.userUid }) as never);
+
+      await supertest(server)
+        .get("/scraps")
+        .expect(200)
+        .expect(request => {
+          expect(request.body.scraps.length).toBeGreaterThanOrEqual(1),
+            expect(request.body.scraps[0].uid).toEqual(scrap.uid);
+        });
+    });
+  });
+});
